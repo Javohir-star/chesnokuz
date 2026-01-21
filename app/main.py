5bb0691a49615fadde93fc3f8f332b8a752c81a0
Javohir-star <jmamadiyorov24@gmail.com>
2026-01-21 18:50:25 +0500

    message

---
+++ app/main.py
@@ -0,0 +1,227 @@
+from datetime import datetime
+import random
+import string
+
+from fastapi import FastAPI
+from fastapi import status
+from fastapi.responses import JSONResponse
+
+from app.schemas import UserCreateRequest, UserCreateResponse
+
+
+app = FastAPI()
+users_db = dict()
+
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    random_string = "".join(random.choices(characters, k=length))
+    return random_string
+
+
+@app.post("/users/create/")
+def user_create():
+    # user_id = str(uuid.uuid4())
+    user_id = random.randrange(1, 10000)
+    name = generate_random_string(10)
+    age = random.randint(1, 60)
+    is_active = random.choice([True, False])
+
+    new_user = {
+        "id": user_id,
+        "name": name,
+        "age": age,
+        "is_active": is_active,
+        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+    users_db[user_id] = new_user
+    return new_user
+
+
+# Serialization (Marshaling)
+
+
+@app.post("/users/create2/", response_model=UserCreateResponse)
+async def user_create2(user_data: UserCreateRequest):
+    user = {
+        "id": random.randint(1, 10000),
+        "name": user_data.name,
+        "age": user_data.age,
+        "is_active": user_data.is_active if user_data.is_active else True,
+    }
+
+    users_db[user["id"]] = user
+
+    return user
+
+@app.post("/users/create3/", response_model=UserCreateResponse)
+async def user_create2(user_data: UserCreateRequest):
+    user = {
+        "id": random.randint(1, 10000),
+        "name": user_data.name,
+        "age": user_data.age,
+        "is_active": user_data.is_active if user_data.is_active else True,
+    }
+
+    users_db[user["id"]] = user
+
+    return user
+
+@app.post("/users/create4/", response_model=UserCreateResponse)
+async def user_create2(user_data: UserCreateRequest):
+    user = {
+        "id": random.randint(1, 10000),
+        "name": user_data.name,
+        "age": user_data.age,
+        "is_active": user_data.is_active if user_data.is_active else True,
+    }
+
+    users_db[user["id"]] = user
+
+    return user
+
+
+@app.get("/users/list/")
+def users_list():
+    return users_db
+
+
+@app.get("/users/{user_id}/")
+def users_detail(user_id: int):
+    try:
+        return users_db[user_id]
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.get("/users/{user_id}2/")
+def users_detail(user_id: int):
+    try:
+        return users_db[user_id]
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.get("/users/{user_id}3/")
+def users_detail(user_id: int):
+    try:
+        return users_db[user_id]
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.get("/users/{user_id}4/")
+def users_detail(user_id: int):
+    try:
+        return users_db[user_id]
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+
+
+@app.put("/users/{user_id}/")
+def user_update(user_id: int):
+    try:
+        user = users_db[user_id]
+        user["name"] = generate_random_string(10)
+        user["age"] = random.randint(1, 60)
+        users_db[user_id] = user
+
+        return user
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.put("/users/{user_id}2/")
+def user_update(user_id: int):
+    try:
+        user = users_db[user_id]
+        user["name"] = generate_random_string(10)
+        user["age"] = random.randint(1, 60)
+        users_db[user_id] = user
+
+        return user
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.put("/users/{user_id}3/")
+def user_update(user_id: int):
+    try:
+        user = users_db[user_id]
+        user["name"] = generate_random_string(10)
+        user["age"] = random.randint(1, 60)
+        users_db[user_id] = user
+
+        return user
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+
+
+@app.delete("/users/{user_id}/")
+def user_delete(user_id: int):
+    try:
+        del users_db[user_id]
+        return JSONResponse(status_code=204)
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"}, status_code=status.HTTP_404_NOT_FOUND
+        )
+
+
+@app.patch("/users/{user_id}/")
+def user_patch(user_id: int, user_data):
+    try:
+        user = users_db[user_id]
+
+        if user_data.name is not None:
+            user["name"] = user_data.name
+
+        if user_data.age is not None:
+            user["age"] = user_data.age
+
+        if user_data.is_active is not None:
+            user["is_active"] = user_data.is_active
+
+        users_db[user_id] = user
+        return user
+
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"},
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+@app.patch("/users/{user_id}2/")
+def user_patch(user_id: int, user_data):
+    try:
+        user = users_db[user_id]
+
+        if user_data.name is not None:
+            user["name"] = user_data.name
+
+        if user_data.age is not None:
+            user["age"] = user_data.age
+
+        if user_data.is_active is not None:
+            user["is_active"] = user_data.is_active
+
+        users_db[user_id] = user
+        return user
+
+    except KeyError:
+        return JSONResponse(
+            content={"error": "User not found"},
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+
+# API nima?
